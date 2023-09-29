3199644e04fc01100027808060f245989e6ad549
kruthi5566@gmail.com <kruthi5566@gmail.com>
2023-09-29 10:39:14 +0530

    kruthika(29-09-23)

--- JournalProjectAngular/src/app/authorindividualsubmissions/authorindividualsubmissions.component.ts
+++ JournalProjectAngular/src/app/authorindividualsubmissions/authorindividualsubmissions.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginserviceService } from '../Services/Login/loginservice.service';
+import { ManuscriptserviceService } from '../Services/manuscriptservice/manuscriptservice.service';
 
 @Component({
   selector: 'app-authorindividualsubmissions',
@@ -7,4 +10,52 @@ import { Component } from '@angular/core';
 })
 export class AuthorindividualsubmissionsComponent {
 
+  IsLoggedIn: boolean = false;
+  RegisterID: any;
+  // This will hold the manuscript submission details
+  manuscriptcontentList: any;
+
+ 
+
+  constructor(
+    private manuscriptService: ManuscriptserviceService,
+    
+  )
+  
+  
+  
+  {
+    if (localStorage.getItem('IsLoggedIn') === 'true') {
+      this.IsLoggedIn = true;
+      this.RegisterID = localStorage.getItem('RegisterID');
+    }
+  }
+  
+
+  ngOnInit() {
+    this.getManuscriptDetailsByRegisterid(); 
+  }
+ 
+  getManuscriptDetailsByRegisterid() {
+    this.manuscriptService.getManusubmissionDetailsByRegisterID( this.RegisterID).subscribe((result: any) => {
+      this.manuscriptcontentList = result;
+      console.log(this.manuscriptcontentList);
+    })
+  }
 }
+
+
+
+
+
+
+
+
+
+
+// showTable2: boolean = false;
+
+// oggleTable2() {
+//   this.showTable2 = !this.showTable2;
+// }
+// author-dashboard.component.ts
